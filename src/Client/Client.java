614ad2ed4c4401a0c3a00ce61745547fb59fd840
ABRAHAM <abraham.gomez228796@potros.itson.edu.mx>
2023-02-17 18:33:11 -0700

    Conexión básica UDP agregada

---
+++ src/Client/Client.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Client;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author spide
+ */
+public class Client {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        final int SERVERPORT = 1000;
+        byte[] buffer = new byte[1024];
+        
+        
+ 
+        
+        try {
+            
+            InetAddress serverDirection = InetAddress.getByName("localhost");
+            DatagramSocket udpSocket = new DatagramSocket();
+            String message = "Hello from the client!";
+            buffer = message.getBytes();
+            DatagramPacket question = new DatagramPacket(buffer,buffer.length,serverDirection,SERVERPORT);
+            udpSocket.send(question);
+            
+            DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+            udpSocket.receive(request);
+            
+            message = new String(request.getData());
+            System.out.println(message);
+            
+            udpSocket.close();
+            
+            
+        } catch (SocketException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+}
--- src/Server/Server.java
+++ src/Server/Server.java
@@ -4,6 +4,14 @@
  */
 package Server;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author spide
@@ -15,6 +23,38 @@ public class Server {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        final int PORT = 1000;
+        byte[] buffer = new byte[1024];
+        
+        try {
+            System.out.println("Start");
+            DatagramSocket udpSocket = new DatagramSocket(PORT);
+            
+            DatagramPacket request = new DatagramPacket(buffer,buffer.length);
+            udpSocket.receive(request);
+            
+            String s = new String(request.getData());
+            System.out.println(s);
+            
+            int portClient = request.getPort();
+            InetAddress direction = request.getAddress();
+            
+            
+            s = "Hello from the server!!";
+            buffer = s.getBytes();
+            
+            
+            DatagramPacket response = new DatagramPacket(buffer, buffer.length,direction,portClient);
+            
+            udpSocket.send(response);
+            
+            
+            
+        } catch (SocketException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
