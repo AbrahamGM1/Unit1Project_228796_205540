a7b5a1ea7826a10adbee416af1da0446eb37f749
ABRAHAM <abraham.gomez228796@potros.itson.edu.mx>
2023-02-17 18:12:53 -0700

    Commit inicial

---
+++ src/unit1_project_228796_205540/main.java
@@ -0,0 +1,14 @@
+package unit1_project_228796_205540;
+
+/**
+ *
+ * @author 00000228796, 00000205540
+ */
+public class main {
+
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
